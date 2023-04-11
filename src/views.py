31b954e94c05dcc01735379bf3962e1d8b427111
AxelCenteno <acentenom1900@alumno.ipn.mx>
2023-04-11 15:22:23 -0600

    Actualización en la ejecución en Flask

--- src/views.py
+++ src/views.py
@@ -1,6 +1,18 @@
-from src import app
-from flask import render_template
+from flask import Blueprint, render_template
 
-@app.route('/', methods=['GET'])
+views = Blueprint('views', __name__)
+
+@views.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@views.route('/perfil', methods=['GET'])
+def perfil():
+    return render_template('perfil.html')
+
+@views.route('/login', methods=['GET'])
+def login():
+    return render_template('inciosesion.html')
+
+
+
