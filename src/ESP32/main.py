163e5ab8216f7a844ccf3ce8c449906436459639
FernandoRosalC <128200031+Rosal1@users.noreply.github.com>
2023-04-12 19:16:43 -0600

    Correción de ejecución

--- src/ESP32/main.py
+++ src/ESP32/main.py
@@ -13,25 +13,26 @@ gpsModule = UART(2, baudrate=9600)
 p2 = Pin(2, Pin.OUT)
 
 while True:
+    p2.off()
     #LED infrarojo
     object_detected = sensor_pin.value() # Leer el valor del pin del sensor infrarrojo
     if object_detected:
         print("¡PuertaAaAaA!")
     else:
         print("¡Todo BIEN!")
-    sleep(0.2)
+        sleep(0.2)
+
     ## GPS
     # read from UART
     gpsModule.readline()
     data = str(gpsModule.readline())
     #data = gps.read()
-    p2.off()
+    
     #Gas
     pot_value = pot.read()
 
     print(pot_value)
-    p2.on()
-    sleep(0.1)
+    sleep(0.2)
     # check if data is not empty
     if data:
         # convert bytes to string and print
